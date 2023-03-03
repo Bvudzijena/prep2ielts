83f75303e87c5f920a5bc07e4877b9b3af739996
Bvudzijena <darlingtonbvudzijena@gmail.com>
2023-03-03 10:50:54 -0800

    all new files

---
+++ src/components/Idioms.js
@@ -0,0 +1,10 @@
+
+function Idioms() {
+    return (
+        <div>
+            <p>Hello World</p>
+        </div>
+    );
+}
+
+export default Idioms;
---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Bootstrap from "react-bootstrap";
+
+function Navbar() {
+    
+    return (
+        
+    );
+}
+
+export default Navbar;
